0b9b1a62f469e06416e12bc8555319742745f986
ean <j19900313@gmail.com>
2024-07-22 23:38:24 +0800

    feat: update vue sdk

--- dist/components/ChatRoom.vue.d.ts
+++ dist/components/ChatRoom.vue.d.ts
@@ -1,2 +1,6 @@
-declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{}>>, {}, {}>;
+declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+    send: (...args: any[]) => void;
+}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{}>> & {
+    onSend?: ((...args: any[]) => any) | undefined;
+}, {}, {}>;
 export default _default;

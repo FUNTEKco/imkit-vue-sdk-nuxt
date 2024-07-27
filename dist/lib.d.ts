e4106ae725b851924a8231e7d235e07b23772e41
Howard Sun <howardsun@csie.io>
2024-07-27 17:56:37 +0800

    feat: update vue sdk

--- dist/lib.d.ts
+++ dist/lib.d.ts
@@ -1,6 +1,7 @@
 import { ModalsContainer } from 'vue-final-modal';
 import { App } from 'vue';
 import { default as ChatRoom } from './components/ChatRoom.vue';
+import { default as ChatRoomInfo } from './components/ChatRoomInfo.vue';
 import { default as Config } from './classes/config';
 import { default as RoomList } from './components/RoomList.vue';
 import { Store } from 'vuex';
@@ -9,7 +10,7 @@ type Options = {
     store: Store<any>;
     config: Config;
 };
-export { RoomList, ChatRoom, ModalsContainer };
+export { RoomList, ChatRoom, ChatRoomInfo, ModalsContainer };
 declare const _default: {
     install: (app: App, options: Options) => Promise<void>;
 };

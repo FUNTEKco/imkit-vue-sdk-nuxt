ccd10314a5a0d8a2a910af364d61d7c213ca0ee4
Howard Sun <howard@funtek.co>
2025-12-03 13:45:59 +0000

    GitLab CI/CD Deploy artifacts to GitHub repo

---
+++ dist/src/components/FloatButton.vue.d.ts
@@ -0,0 +1,13 @@
+interface Props {
+    massTextingEnabled?: boolean;
+}
+declare const _default: import('vue').DefineComponent<Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+    createFolder: () => any;
+    massTexting: () => any;
+}, string, import('vue').PublicProps, Readonly<Props> & Readonly<{
+    onCreateFolder?: (() => any) | undefined;
+    onMassTexting?: (() => any) | undefined;
+}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {
+    floatButtonRef: HTMLDivElement;
+}, HTMLDivElement>;
+export default _default;

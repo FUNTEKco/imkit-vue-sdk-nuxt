0e05bc6cb804b1ee9cf7a40784a240b07ab0b80a
Howard Sun <howard@funtek.co>
2026-02-28 12:28:40 +0000

    GitLab CI/CD Deploy artifacts to GitHub repo

--- dist/src/components/RoomList.vue.d.ts
+++ dist/src/components/RoomList.vue.d.ts
@@ -1,4 +1,6 @@
 declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {
+    panelRef: HTMLDivElement;
+    menuTriggerRef: HTMLDivElement;
     searchInput: HTMLInputElement;
 }, any>;
 export default _default;
